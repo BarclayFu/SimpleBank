541925fc54c87a3b4f675c6b0cb2c6f15acbd3c4
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-09-27 00:08:24 -0400

    write unit test(main)

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -3,7 +3,7 @@
 //   sqlc v1.22.0
 // source: account.sql
 
-package sqlc
+package db
 
 import (
 	"context"
@@ -37,6 +37,16 @@ func (q *Queries) CreateAccount(ctx context.Context, arg CreateAccountParams) (A
 		&i.CreatedAt,
 	)
 	return i, err
+}
+
+const deleteAccount = `-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1
+`
+
+func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteAccount, id)
+	return err
 }
 
 const getAccount = `-- name: GetAccount :one
@@ -98,7 +108,7 @@ func (q *Queries) ListAccounts(ctx context.Context, arg ListAccountsParams) ([]A
 	return items, nil
 }
 
-const updateAccount = `-- name: UpdateAccount :exec
+const updateAccount = `-- name: UpdateAccount :one
 UPDATE accounts
 SET balance = $2
 WHERE id = $1
@@ -110,7 +120,15 @@ type UpdateAccountParams struct {
 	Balance int64
 }
 
-func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) error {
-	_, err := q.db.ExecContext(ctx, updateAccount, arg.ID, arg.Balance)
-	return err
+func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error) {
+	row := q.db.QueryRowContext(ctx, updateAccount, arg.ID, arg.Balance)
+	var i Account
+	err := row.Scan(
+		&i.ID,
+		&i.Owner,
+		&i.Balance,
+		&i.Currency,
+		&i.CreatedAt,
+	)
+	return i, err
 }
