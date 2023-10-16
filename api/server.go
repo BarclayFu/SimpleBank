698ef6719f03483fe6adeaf112b8cc389585492f
BarclayFu <50268764+BarclayFu@users.noreply.github.com>
2023-10-15 23:31:06 -0400

    update by using gin

--- api/server.go
+++ api/server.go
@@ -14,8 +14,16 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
-	router.POST("/accounts", server.router.createAccount)
+	router.POST("/accounts", server.createAccount)
 
 	server.router = router
 	return server
+}
+
+func (server *Server) start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
 }
